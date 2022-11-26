e783e9edc93ce07c2cd69a7e0457a603dbecdc97
+ <+>
2022-11-26 16:39:47 +0300

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :instructors, only: [:show] do
+    # nested resource for students
+    resources :students, only: [:show, :index]
+  end 
+  resources :instructors, only: [:show, :index, :create, :update, :destroy]
+
+  resources :students, only: [:show, :index, :create, :update, :destroy]
 end
